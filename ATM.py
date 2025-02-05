fc8689f1d9c7fa757085fd123d6c1cbf1e46d7fb
rakshitha2427-ctrl <rakshitharakshi2427@gmail.com>
2025-02-05 14:28:44 +0530

    ss

---
+++ ATM.py
@@ -0,0 +1,15 @@
+print("welcome to sbi")
+print("insert the atm card to aim machine")
+pincode="2427"
+print(pincode)
+if x==1:
+    print("engalish language")
+elif x==2:
+    print("hindhi language")
+else:
+    print("kannada language")
+amount="1000"
+print(amount)
+drawamount=("500")
+print(drawamount)
+
